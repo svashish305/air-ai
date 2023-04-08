05e78cd49819786bd61400bf3b1d9c9933d9f4fa
Shubhashish Verma <svashish305@gmail.com>
2023-04-09 03:03:31 +0530

    added pm2.5 scale

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import './App.scss'
 function App() {
   const [prompt, setPrompt] = useState("");
   const [chat, setChat] = useState<any>([]);
+  const [isAi, setIsAi] = useState(false);
 
   const handleKeyDown = (e: any) => {
     if (e.key === "Enter") {
@@ -17,6 +18,7 @@ function App() {
     e.preventDefault();
 
     // add user's chat to chat list
+    setIsAi(false);
     setChat((chat: any) => [...chat, { isAi: false, value: prompt, uniqueId: generateUniqueId() }]);
     setPrompt("");
 
@@ -35,12 +37,14 @@ function App() {
         const parsedData = data.data.trim();
         
         // add bot's chat to chat list
+        setIsAi(true);
         setChat((chat: any) => [...chat, { isAi: true, value: parsedData, uniqueId: generateUniqueId() }]);
       } else {
         const err = await response.text();
         alert(err);
       }
     } catch (error) {
+      setIsAi(false);
       console.log(error);
       alert(error);
     }
@@ -89,18 +93,44 @@ function App() {
         </form>
       </>
       <>
-        {/* Visual Indicator of AQI */}
-        <div>
-          <div style={{textAlign: "center", padding: "1rem", color: "white"}}>AQI Scale</div>
-          <div className="aqiScaleContainer">
-            <div className="aqiScaleBox" style={{backgroundColor: 'maroon'}}>Hazardous (&gt; 300)</div>
-            <div className="aqiScaleBox" style={{backgroundColor: 'purple'}}>Very Unhealthy (201-300)</div>
-            <div className="aqiScaleBox" style={{backgroundColor: 'red'}}>Unhealthy (151-200)</div>
-            <div className="aqiScaleBox" style={{backgroundColor: 'orange'}}>Unhealthy for Sensitive Groups (101-150)</div>
-            <div className="aqiScaleBox" style={{backgroundColor: 'yellow'}}>Moderate (51-100)</div>
-            <div className="aqiScaleBox" style={{backgroundColor: 'green'}}>Good (0-50)</div>
+        {((!prompt || !prompt.length)) && isAi && <div>
+          {/* Visual Indicator of AQI */}
+          <div>
+            <div style={{textAlign: "center", padding: "1rem", color: "white"}}>AQI Scale</div>
+            <div className="aqiScaleContainer">
+              <div className="aqiScaleBox" style={{backgroundColor: 'maroon'}}>Hazardous (&gt; 300)</div>
+              <div className="aqiScaleBox" style={{backgroundColor: 'purple'}}>Very Unhealthy (201-300)</div>
+              <div className="aqiScaleBox" style={{backgroundColor: 'red'}}>Unhealthy (151-200)</div>
+              <div className="aqiScaleBox" style={{backgroundColor: 'orange'}}>Unhealthy for Sensitive Groups (101-150)</div>
+              <div className="aqiScaleBox" style={{backgroundColor: 'yellow'}}>Moderate (51-100)</div>
+              <div className="aqiScaleBox" style={{backgroundColor: 'green'}}>Good (0-50)</div>
+            </div>
           </div>
-        </div>
+
+          {/* Visual Indicator of PM2.5 Concentration averaged over 1 hour */}
+          <div>
+            <div style={{textAlign: "center", marginTop: "1rem", padding: "1rem", color: "white"}}>PM2.5 µg/m3 Concentration Scale (averaged over 1 hour)</div>
+            <div className="aqiScaleContainer">
+              <div className="aqiScaleBox" style={{backgroundColor: 'maroon'}}>Extremely Poor (&gt; 300)</div>
+              <div className="aqiScaleBox" style={{backgroundColor: 'red'}}>Very Poor (100-300)</div>
+              <div className="aqiScaleBox" style={{backgroundColor: 'orange'}}>Poor (50-100)</div>
+              <div className="aqiScaleBox" style={{backgroundColor: 'yellow'}}>Fair (25-50)</div>
+              <div className="aqiScaleBox" style={{backgroundColor: 'green'}}>Good (0-25)</div>
+            </div>
+          </div>
+
+          {/* Visual Indicator of PM2.5 Concentration averaged over 24 hours */}
+          <div>
+            <div style={{textAlign: "center", marginTop: "1rem", padding: "1rem", color: "white"}}>PM2.5 µg/m3 Concentration Scale (averaged over 24 hours)</div>
+            <div className="aqiScaleContainer">
+              <div className="aqiScaleBox" style={{backgroundColor: 'maroon'}}>Extremely Poor (&gt; 150)</div>
+              <div className="aqiScaleBox" style={{backgroundColor: 'red'}}>Very Poor (50-150)</div>
+              <div className="aqiScaleBox" style={{backgroundColor: 'orange'}}>Poor (25-50)</div>
+              <div className="aqiScaleBox" style={{backgroundColor: 'yellow'}}>Fair (12.5-25)</div>
+              <div className="aqiScaleBox" style={{backgroundColor: 'green'}}>Good (0-12.5)</div>
+            </div>
+          </div>
+        </div>}
       </>
     </div>
   )
